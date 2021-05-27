819fbc087a26adc31ea28f170a7e734007ead10d
Taewoo Kim <embedded61@gmail.com>
2021-05-27 20:20:36 +0900

    Add: users app url 및 UserView 초기설정 추가
    1. url 설정 및 확인을 위해서 UserView 기본적인 구조 생성

--- students/taewookim/users/views.py
+++ students/taewookim/users/views.py
@@ -1,1 +1,8 @@
-from django.shortcuts import render
+from django.views         import View
+from django.http.response import JsonResponse
+
+from .models              import User
+
+class UserView(View):
+    def post(self, request):
+        return JsonResponse({"result": "CREATED"}, status=201)

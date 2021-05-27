52d2c3658192120a5dfd970335ce78071bec4346
Taewoo Kim <embedded61@gmail.com>
2021-05-27 20:03:29 +0900

    Add: User model 추가
    1. model 구성내용
     - email : emailfield로 구성
     - password :
     - nickname : Null 허용하나 중복이 안되도록 함.  (추가 정보)
     - phone_number : nickname과 동일

--- students/taewookim/users/models.py
+++ students/taewookim/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email        = models.EmailField(max_length=50, unique=True, null=False, blank=False)
+    password     = models.CharField(max_length=30, null=False, blank=False)
+    nickname     = models.CharField(max_length=40, unique=True, null=True, blank=False)
+    phone_number = models.CharField(max_length=13, unique=True, null=True, blank=False)
+
+    class Meta:
+        db_table = "users"

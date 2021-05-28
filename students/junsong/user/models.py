8f37f5c355ad6c30bde47bffaad574b4464c6bd2
riassuc <riassuc@gmail.com>
2021-05-28 12:03:12 +0900

    Add: UserApp models.py 생성

--- students/junsong/user/models.py
+++ students/junsong/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField(max_length=128)
+    password = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=20, blank=True)
+    contact = models.IntegerField(null=True)
+
+    class Meta:
+        db_table = 'users'

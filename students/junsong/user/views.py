d200490cfa34a91b471a815553946dd440bd81ee
riassuc <riassuc@gmail.com>
2021-05-28 20:07:47 +0900

    Modify: status코드, elif -> if 수정

--- students/junsong/user/views.py
+++ students/junsong/user/views.py
@@ -18,17 +18,17 @@ class SignUp(View):
             if ('@' or '.') not in email:
                 return JsonResponse({"message": "올바르지 않은 이메일 형식입니다."}, status=400)
 
-            elif User.objects.filter(email=email).exists():
-                return JsonResponse({"message": "이미 존재하는 계정입니다."}, status=400)
+            if User.objects.filter(email=email).exists():
+                return JsonResponse({"message": "이미 존재하는 계정입니다."}, status=409)
 
-            elif len(password) < 8:
+            if len(password) < 8:
                 return JsonResponse({"message": "비밀번호는 8자리 이상으로 만들어주세요."}, status=400)
 
-            elif (contact != '') and User.objects.filter(contact=contact).exists():
-                return JsonResponse({"message": "중복된 연락처입니다."}, status=400)
+            if (contact != '') and User.objects.filter(contact=contact).exists():
+                return JsonResponse({"message": "중복된 연락처입니다."}, status=409)
 
-            elif (nickname != '') and User.objects.filter(nickname=nickname).exists():
-                return JsonResponse({"message": "중복된 닉네임입니다."}, status=400)
+            if (nickname != '') and User.objects.filter(nickname=nickname).exists():
+                return JsonResponse({"message": "중복된 닉네임입니다."}, status=409)
 
             else:
                 User.objects.create(

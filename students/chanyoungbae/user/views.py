011a9fb39da7a3fa59b953a04d33cb9692c6b02c
cj4207 <cksdud1226@gmail.com>
2021-05-28 21:37:01 +0900

    Add : NULL 값 추가 처리

--- students/chanyoungbae/user/views.py
+++ students/chanyoungbae/user/views.py
@@ -3,6 +3,7 @@ import re
 
 from django.views import View
 from django.http import JsonResponse
+from django.db.models import Q
 from .models import User
 
 class SignUpView(View):
@@ -12,28 +13,23 @@ class SignUpView(View):
 
             email     = data['email']
             password  = data['password']
-            phone_num = data['phone_num']
-            name      = data['name']
+            phone_num = data.get('phone_num')
+            name      = data.get['name']
 
             email_regex    = '^([\w\.\-_]+)?\w+@[\w]+(\.\w+){1,}$'
             password_regex = '^([a-zA-Z0-9~!@#$%^&*()_+]).{7,}$'
 
+            if phone_num=='' or name=='':
+                return JsonResponse({"message":"EMTPY_VALUE"}, status = 400)
+
             if not email or not password:
-                return JsonResponse({"message": "KEY_ERROR"}, status = 400)
+                return JsonResponse({"message":"KEY_ERROR"}, status = 400)
 
             if not re.match(email_regex, email):
                 return JsonResponse({"message":"INVALID_VALUE"}, status = 400)
-
-            if User.objects.filter(email=email):
-                return JsonResponse({"message":"DUPLICATE EMAIL"}, status = 400)
             
-            if phone_num:
-                if User.objects.filter(phone_num=phone_num):
-                    return JsonResponse({"message":"DUPLICATE PHONE NUMBER"}, status = 400)
-
-            if name:    
-                if User.objects.filter(name=name):
-                    return JsonResponse({"message":"DUPLICATE NAME"}, status = 400)
+            if User.objects.filter(phone_num=phone_num, name=name, email=email):
+                return JsonResponse({"message":"DUPLICATE_VALUE"}, status = 409)
 
             if not re.match(password_regex, password):
                 return JsonResponse({"message":"SHORT PASSWORD"}, status = 400)

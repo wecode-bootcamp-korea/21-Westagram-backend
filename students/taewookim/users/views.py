97983ec1d08926a6d355b1a8b8414ff4df751341
Taewoo Kim <embedded61@gmail.com>
2021-05-28 14:02:03 +0900

    Add: 회원가입 로직 추가
    1. 이메일
     - 이메일 양식 - 정규식으로 체크
     - 중복체크
    2. 비밀번호
     - 8자리 이상체크 - 정규식으로 체크
    3. 닉네임
     - 중복 체크
     - 빈값 및 2자리 이상인지  체크 - 정규식
    4. 전화번호
     - 중복 및 빈값 체크
     - 빈값 및 양식 체크 - 정규식
    
    * 추가 정보인 닉네임과 전화번호는 입력이 안되도 처리 할 수 있도록
      get()을 이용해서 받도록 함.
    * Key가 전달되지 않으면 {message": "KEY_ERROR} status code 400 반환
      처리함.
    * 그외에러는 상황에 맞는 메시지로 반환 처리함
    * 정상적으로 저장 시, {"message": "SUCCESS"}, status code 201 반환
      처리함.

--- students/taewookim/users/views.py
+++ students/taewookim/users/views.py
@@ -1,8 +1,54 @@
-from django.views         import View
-from django.http.response import JsonResponse
+import json, re
+from json.decoder           import JSONDecodeError
+
+from django.views           import View
+from django.http.response   import JsonResponse
 
 from .models              import User
 
+EMAIL_REGEX    = '^([a-z0-9_+.-]+@([a-z0-9-]+\.)+[a-z0-9]{2,4}){1,50}$'
+PASSWORD_REGEX = '^.{8,30}$'
+NICKNAME_REGEX = '^.{2,10}$'
+PHONE_REGEX    = '^01[016789]\-\d{3,4}\-\d{4}$'
+
 class UserView(View):
     def post(self, request):
-        return JsonResponse({"result": "CREATED"}, status=201)
+        try:
+            data         = json.loads(request.body)
+            email        = data['email']
+            password     = data['password']
+            nickname     = data.get('nickname')
+            phone_number = data.get('phone_number')
+
+            if re.compile(EMAIL_REGEX).match(email) == None:
+                return JsonResponse({"result": "INVALIED_EMAIL"}, status=400)
+
+            if re.compile(PASSWORD_REGEX).match(password) == None:
+                return JsonResponse({"result": "INVALIED_PASSWORD"}, status=400)
+
+            if User.objects.filter(email=email).exists() :
+                return JsonResponse({"result": "DUPLICATED_EMAIL"}, status=400)
+
+            if nickname is not None:
+                if not re.compile(NICKNAME_REGEX).match(nickname):
+                    return JsonResponse({"result": "INVALIED_NICKNAME"}, status=400)
+
+                if User.objects.filter(nickname=nickname).exists() :
+                    return JsonResponse({"result": "DUPLICATED_NICKNAME"}, status=400)
+
+            if phone_number is not None:
+                if not re.compile(PHONE_REGEX).match(phone_number):
+                    return JsonResponse({"result": "INVALIED_PHONE_NUMBER"}, status=400)
+
+                if User.objects.filter(phone_number=phone_number).exists() :
+                    return JsonResponse({"result": "DUPLICATED_PHONE_NUMBER"}, status=400)
+                
+            User.objects.create(email=email, password=password, phone_number=phone_number, nickname=nickname)
+
+            return JsonResponse({"result": "CREATED"}, status=201)
+
+        except JSONDecodeError:
+            return JsonResponse({"result": "EMPTY_BODY_DATA"}, status=400)
+
+        except KeyError:
+            return JsonResponse({"result": "KEY_ERROR"}, status=400)

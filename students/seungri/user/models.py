9ea6fda6242c68b280f42e249f1321583e849f98
최승리 <apol.lun058@gmail.com>
2021-05-28 10:11:26 +0900

    Mission2:회원가입 엔드포인트_ver_01

--- students/seungri/user/models.py
+++ students/seungri/user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import IntegerField
 
-# Create your models here.
+class User(models.Model):
+    email     = models.EmailField(max_length=128, unique=True)
+    password  = models.CharField(max_length=45)
+    phone_num = models.CharField(max_length=45, unique=True, blank=True, null=True)
+    nick_name  = models.CharField(max_length=45, unique=True, blank=True, null=True)
+    
+    class Meta:
+        db_table = 'user'
+
--- students/seungri/user/views.py
+++ students/seungri/user/views.py
@@ -1,1 +1,34 @@
+import json
 
+from django.views import View
+from django.http  import JsonResponse
+from django.db    import IntegrityError
+
+from .models import User
+
+class UserListView(View):
+    def post(self, request):
+        try:
+            data      = json.loads(request.body)
+            email     = data['email']
+            password  = data['password']
+            phone_num = data['phone_num']
+            nick_name = data['nick_name']
+
+            if '.' not in email:
+                return JsonResponse({"message": "EMAIL_KEY_ERROR"}, status =  400)
+            elif '@' not in email:
+                return JsonResponse({"message": "EMAIL_KEY_ERROR"}, status =  400)
+            
+            if len(password) < 8:
+                return JsonResponse({"message": "PASSWORD_SHORT"}, status =  400)
+
+            User.objects.create(email=email,
+                                password=password,
+                                phone_num=phone_num,
+                                nick_name=nick_name)
+            return JsonResponse({'message' : "SUCCESS!"}, status = 201)
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+        except IntegrityError:
+            return JsonResponse({"message": "DUPLICATE_ENTRY_KEY_ERROR"}, status=400)

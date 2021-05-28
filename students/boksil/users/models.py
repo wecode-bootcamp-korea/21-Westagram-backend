c4961785521823513cf5a575412442b7c88f65dc
HWANG BOK SIL <tlatms0908@gmail.com>
2021-05-28 13:55:51 +0900

    Add: userApp & userTable 생성

---
+++ students/boksil/users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class User(models.Model):
+    mobile   = models.IntegerField(),
+    nickname = models.CharField(max_length=45),
+    email    = models.EmailField(max_length=45),
+    password = models.CharField(max_length=90)
+
+    class Meta:
+        db_table = 'users'
--- students/boksil/westagram/settings.py
+++ students/boksil/westagram/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
-    'user'
+    # 'corsheaders',
+    'users'
+
 ]
 
 MIDDLEWARE = [

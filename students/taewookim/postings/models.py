938df951ea3cc486f980dee3ed7d6973422a819b
Taewoo Kim <embedded61@gmail.com>
2021-05-30 19:10:14 +0900

    Add: Posting및 PostingImage model 생성 및 PostingView로직 추가(진행중)
    1. Model생성: Posting과 PostingImage 이미지는 1:N관계로 구성
       (하나의 posting의 여러개의 Image)
    2. PostingView 초기 로직 추가(진행중)
     - posting등록(post) 및 posting 목록 조회(get) 기본 로직 작성 완료
     - image 등록 및 결과에 추가하는 작업은 진행 중

--- students/taewookim/postings/models.py
+++ students/taewookim/postings/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.db                       import models
+from django.db.models.fields.related import ForeignKey
 
-# Create your models here.
+class Posting(models.Model):
+    user       = ForeignKey('users.user', on_delete=models.CASCADE)
+    title      = models.fields.CharField()
+    main_text  = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class PostingImage(Posting):
+    posting = ForeignKey(Posting, on_delete=models.CASCADE)
+    url     = models.CharField(max_length=200)
+    
+    class Meta():
+        db_table = 'posting_images'
--- students/taewookim/postings/views.py
+++ students/taewookim/postings/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+import json
+from json.decoder                 import JSONDecodeError
 
-# Create your views here.
+from django.views                 import View
+from django.http.response         import JsonResponse
+from django.db.models.expressions import F
+
+from users.models                 import post
+from .models                      import Posting
+
+class PosingView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request)
+            user = User.Objects.get(email=data['email'])
+
+            Posting.objects.create(
+                user      = user,
+                title     = data['title'],
+                main_text = data['main_text']
+                )
+
+            JsonResponse({'message': 'CREATED'}, status=201)
+
+        except ValueError:
+            return JsonResponse({'message': 'INVALIED_DATA'}, status=400)
+
+        except JSONDecodeError:
+            return JsonResponse({'message': 'NO_BODY_DATA'}, status=400)
+
+        except User.DoesNotExist:
+            return JsonResponse({'message': 'NO_EXIST_USER'}, status=409)
+
+        def get(self, request):
+            result = [
+                posting for posting in Posting.objects.all().values(
+                'title', 'main_text', 'created_at', user=F('user__email')
+                )]
+                
+            JsonResponse({'message': result}, status=200)

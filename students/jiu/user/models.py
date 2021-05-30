8f2d586885dc320d2712b205db29a551a32daacb
ttakku-park <ttakku.park@gmail.com>
2021-05-30 18:22:08 +0900

    Add : models unique,null 추가 , view 키/중복except 추가 , url 변경

--- students/jiu/user/models.py
+++ students/jiu/user/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model) :
 
     nickname    = models.CharField(max_length=30)
-    email       = models.EmailField()
-    password    = models.CharField(max_length=20)
-    phonenumber = models.CharField(max_length=11)
+    email       = models.EmailField(max_length=50, unique=True, null=False)
+    password    = models.CharField(max_length=20,  unique=True, null=False)
+    phonenumber = models.CharField(max_length=11,  unique=True)
 
     class Meta:
         db_table = 'users'
--- students/jiu/user/views.py
+++ students/jiu/user/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from django.http  import JsonResponse
+from django.db    import IntegrityError
+from .models      import User
+
+
+class NewUserView(View) :
+
+    def post(self, request) :
+
+        try:
+            data = json.loads(request.body)
+
+            User.objects.create(
+
+                nickname   = data['nickname'],
+                email      = data['email'],
+                password   = data['password'],
+                phonenumber= data['phonenumber']
+
+                )
+            return JsonResponse({'message':'SUCCESS'}, status=201)
+
+        except KeyError :
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
+        except IntegrityError : # 중복예외처리
+            return JsonResponse({"message": "INTERGRITY_ERROR"}, status=400)
+
+        
+
+

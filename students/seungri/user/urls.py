38e8bb636b1ab20b11e5a5e37a8b929410379b79
최승리 <apol.lun058@gmail.com>
2021-05-31 18:15:34 +0900

    [최승리] Mission2: 회원가입 엔드포인트 완료_수정

--- students/seungri/user/urls.py
+++ students/seungri/user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserJoinIn
+from .views import UserJoinIn, UserLogIn
 
 urlpatterns = [
-    path('/users', UserJoinIn.as_view())
+    path('/joinin', UserJoinIn.as_view()),
+    path('/login', UserLogIn.as_view())
 ]
--- students/seungri/user/views.py
+++ students/seungri/user/views.py
@@ -4,6 +4,7 @@ import re
 from django.views import View
 from django.http  import JsonResponse
 from django.db    import IntegrityError
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import User
 
@@ -16,20 +17,37 @@ class UserJoinIn(View):
             phone_num = data["phone_num"]
             nick_name = data["nick_name"]
             
-            email_regex = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+            email_regex = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
 
-            if not email_regex.match(email):
+            if not re.match(email_regex, data['email']):
                 return JsonResponse({"message": "EMAIL_FORM_ERROR"}, status =  400)
 
             if len(password) < 8 or len(password) > 15:
                 return JsonResponse({"message": "PASSWORD_ERROR"}, status=400)
 
-            User.objects.create(email     = email,
-                                password  = password,
-                                phone_num = phone_num,
-                                nick_name = nick_name)
+            User.objects.create(
+                email     = email,
+                password  = password,
+                phone_num = phone_num,
+                nick_name = nick_name
+                )
             return JsonResponse({"message": "SUCCESS!"}, status=200)
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
         except IntegrityError:
             return JsonResponse({"message": "DUPLICATE_ENTRY_KEY_ERROR"}, status=400)
+
+class UserLogIn(View):
+    def post(self, request):
+        try:
+            data      = json.loads(request.body)
+            email     = data["email"]
+            password  = data["password"]
+
+            if email == "" or password == "":
+                return JsonResponse({"message": "KEY_ERROR"}, status=400)
+            elif User.objects.get(email=email, password=password):
+                return JsonResponse({"message": "SUCCESS!"}, status=200)
+
+        except ObjectDoesNotExist:
+            return JsonResponse({"message": "INVALID_USER"}, status=401)

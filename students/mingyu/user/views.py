c10ee75c6aff3735671881b29ebcca056b99d458
김민규 <skystar_pisces@gimmingyuui-MacBookAir.local>
2021-05-31 15:03:27 +0900

    Add: Misson 2 수정 완료

--- students/mingyu/user/views.py
+++ students/mingyu/user/views.py
@@ -13,17 +13,15 @@ class SignupView(View):
             data = json.loads(request.body)
             
             email_regex = re.compile(r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-
+            password_regex = re.compile(r'[A-Za-z0-9@#$]{8,}')
+            phone_number_regex = re.compile('\d{2,3}-\d{3,4}-\d{4}')
+            
             if not email_regex.search(data['email']):
                 return JsonResponse({"message" : "INVALID_EMAIL"}, status=400)
-
-            password_regex = re.compile(r'[A-Za-z0-9@#$]{8,}')
 
             if not password_regex.search(data['password']):
                 return JsonResponse({'message': 'INVAILD_PASSWORD'})
         
-            phone_number_regex = re.compile('\d{2,3}-\d{3,4}-\d{4}')
-
             if not phone_number_regex.search(data['phone_number']):
                 return JsonResponse({'message': 'INVALID_PHONE_NUMBER'})
 
@@ -32,7 +30,7 @@ class SignupView(View):
                 password     = data['password'],
                 nickname     = data['nickname'],
                 phone_number = data['phone_number']
-                )
+            )
 
             return JsonResponse({'message': 'SUCCESS'}, status=201)
         

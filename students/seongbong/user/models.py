02a8e1bb24a076d723e6cc1d7e502f27a8034431
SSABOODA <69753846+SSABOODA@users.noreply.github.com>
2021-05-31 16:49:15 +0900

    modify: review 수정

--- students/seongbong/user/models.py
+++ students/seongbong/user/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class User(models.Model):
     email        = models.EmailField(max_length=50, unique=True)
     password     = models.CharField(max_length=300)
-    phone_number = models.CharField(max_length=20, unique=True, null=True)
-    nickname     = models.CharField(max_length=20, unique=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    nickname     = models.CharField(max_length=20, blank=True)
     
-    class Meta():
+    class Meta:
         db_table = 'users'
--- students/seongbong/user/views.py
+++ students/seongbong/user/views.py
@@ -5,28 +5,38 @@ from django.db    import IntegrityError
 from django.views import View 
 from .models      import User 
 
-email_regex = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-
 class UserSignUp(View):
     def post(self, request):
+        email_regex = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        data = json.loads(request.body)
+        signup_data = User.objects.all()
         try:
-            data = json.loads(request.body)
-            if request.method == 'POST' and len(data['password']) < 8:
-                return JsonResponse({'message': 'PASSWORD_ERROR'}, status= 400)
+            if not re.match(email_regex, data['email']):
+                return JsonResponse({'message': 'EMAIL_ERROR'}, status=400) 
             
-            if not re.compile(email_regex).match(data['email']):
-                return JsonResponse({'message': 'EMAIL_ERROR'}, status= 400)   
+            if len(data['password']) < 8:
+                return JsonResponse({'message': 'PASSWORD_ERROR'}, status=400)
+            
+            if signup_data.filter(nickname = data['nickname']).exists(): 
+                return JsonResponse({'message': 'UNIQUE_ERROR'}, status=400)
+
+            if signup_data.filter(phone_number = data['phone_number']).exists():
+                return JsonResponse({'message': 'UNIQUE_ERROR'}, status=400)    
+
             User.objects.create(
-                        email        = data['email'],
-                        password     = data['password'],
-                        nickname     = data['nickname'],
-                        phone_number = data['phone_number']
-                        )
+                email        = data['email'],
+                password     = data['password'],
+                nickname     = data['nickname'],
+                phone_number = data['phone_number']
+            )
         except KeyError:
-            return JsonResponse({'message': 'KEY_ERROR'}, status= 400)
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
         except IntegrityError:
-            return JsonResponse({'message': 'UNIQUE_ERROR'}, status= 400)
+            return JsonResponse({'message': 'UNIQUE_ERROR'}, status=400)
         
-        return JsonResponse({'message': 'SUCCESS'}, status= 201)
+        return JsonResponse({'message': 'SUCCESS'}, status=201)
         
     
+
+
+

c4823047d0064041544b1bf0cd159542bb61cf29
yu <yu@yuui-MacBookPro.local>
2021-05-31 16:17:48 +0900

    MODIFY: models.py 및 views.py 수정

--- students/byunggun/user/models.py
+++ students/byunggun/user/models.py
@@ -3,11 +3,11 @@ from django.db import models
 class User(models.Model):
     email        = models.CharField(max_length=100, unique=True)
     password     = models.CharField(max_length=45, null=False)
-    phone_no     = models.CharField(max_length=45, unique=True)
-    full_nm      = models.CharField(max_length=45, null=False)
-    user_nm      = models.CharField(max_length=100, unique=True)
-    reg_dte      = models.DateTimeField(auto_now_add=True)
-    withdraw_dte = models.DateTimeField(null=True)
+    phone_number = models.CharField(max_length=45, unique=True)
+    full_name    = models.CharField(max_length=45, null=False)
+    user_name    = models.CharField(max_length=100, unique=True)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    deleted_at   = models.DateTimeField(null=True)
 
     class Meta:
         db_table = 'users'

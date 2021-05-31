ecfc96781456a891afb557e3d721c632d4e685b7
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2021-05-31 17:07:42 +0900

    Change: view.py 수정

--- students/hyojin/user/models.py
+++ students/hyojin/user/models.py
@@ -7,12 +7,6 @@ class User(models.Model):
     password     = models.CharField(max_length=45, validators=[validate_password])
     phone_number = models.CharField(max_length=30, unique=True, blank=True, null=True)
     nickname     = models.CharField(max_length=45, unique=True, blank=True, null=True)
-
-    def check_blank(self, value):
-        if value == "":
-            return None
-        else: 
-            return value
  
     class Meta:
         db_table='users'
--- students/hyojin/user/views.py
+++ students/hyojin/user/views.py
@@ -4,7 +4,6 @@ from django.db.models.fields import IntegerField
 from django.views           import View
 from django.http            import JsonResponse
 from django.core.exceptions import ValidationError
-from django.db              import IntegrityError
 
 from .models import User
 
@@ -31,10 +30,11 @@ class NewUserView(View):
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
 
-        except IntegrityError:
-            return JsonResponse({"message":"이미 존재하는 값 입니다."}, status=400)
-
         except ValidationError as e: 
             return JsonResponse({'message':e.message_dict}, status=400)
 
-       
+    def check_blank(self, value):
+        if value == "":
+            return None
+        else: 
+            return value

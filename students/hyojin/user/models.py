0f1adde31f3c1f2415d9ef155b5495f88774d632
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2021-05-31 20:52:42 +0900

    Create: 로그인 엔드포인트 완료

--- students/hyojin/user/models.py
+++ students/hyojin/user/models.py
@@ -4,7 +4,7 @@ from .validations import validate_email, validate_password
 
 class User(models.Model):
     email        = models.CharField(max_length=70, unique=True, validators=[validate_email])
-    password     = models.CharField(max_length=45, validators=[validate_password])
+    password     = models.CharField(max_length=256, validators=[validate_password])
     phone_number = models.CharField(max_length=30, unique=True, blank=True, null=True)
     nickname     = models.CharField(max_length=45, unique=True, blank=True, null=True)
  
--- students/hyojin/user/views.py
+++ students/hyojin/user/views.py
@@ -1,10 +1,14 @@
 import json
+import jwt
+from jwt.algorithms import Algorithm
+import bcrypt 
 
 from django.views           import View
 from django.http            import JsonResponse
 from django.core.exceptions import ValidationError
 
 from .models import User
+from my_settings import SECRET_KEY, ALGORITHM
 
 class NewUserView(View):
     def post(self, request):
@@ -18,6 +22,7 @@ class NewUserView(View):
                 nickname     = data.get('nickname')
             )
 
+            user.password     = self.make_hash_value(user.password)
             user.phone_number = self.check_blank(user.phone_number)
             user.nickname     = self.check_blank(user.nickname)
             
@@ -32,8 +37,38 @@ class NewUserView(View):
         except ValidationError as e: 
             return JsonResponse({'message':e.message_dict}, status=400)
 
+    def make_hash_value(self, value):
+        result = bcrypt.hashpw(value.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+        return result
+
     def check_blank(self, value):
         if value == "":
             return None
         else: 
             return value
+
+class SignInView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            
+            email    = data['email']
+            password = data['password']
+
+            user = User.objects.get(email=email)
+
+            if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                return JsonResponse({'message':'INVALID_USER'}, status=401)
+            
+            data = {'user_id':user.id}
+            
+            token = jwt.encode(data, SECRET_KEY, ALGORITHM)
+
+            return JsonResponse({'access_token':token}, status=200)
+
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+        
+        except User.DoesNotExist:
+            return JsonResponse({'message':'INVALID_USER'}, status=401)

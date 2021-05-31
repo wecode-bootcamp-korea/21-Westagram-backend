e648f70f208dcf2291e19ecae312dcfcf21837ac
HWANG BOK SIL <tlatms0908@gmail.com>
2021-05-31 13:55:39 +0900

    Modify: SignUp View email 정규식 적용

--- students/boksil/users/models.py
+++ students/boksil/users/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 
 class User(models.Model):
-    email    = models.EmailField(max_length=45) # unique=True 중복없게 만들어줌
-    # null=True 라고 지정했는데 ("Column 'mobile' cannot be null")
-    mobile   = models.CharField(max_length=20, null=True) # null=True
-    nickname = models.CharField(max_length=45, null=True) # null=True
+    email    = models.EmailField(max_length=45)
+    mobile   = models.CharField(max_length=20, null=True, blank=False) # null=True
+    nickname = models.CharField(max_length=45, null=True, blank=False) # null=True
     password = models.CharField(max_length=90) 
 
     class Meta():
--- students/boksil/users/views.py
+++ students/boksil/users/views.py
@@ -1,9 +1,11 @@
-import json
+import json, re
 
 from django.views import View
 from django.http  import JsonResponse
 
 from .models import User
+
+email_regex = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
 
 class SignupView(View):
     def post(self, request):
@@ -16,23 +18,21 @@ class SignupView(View):
 
             # unique=True가 안 먹혀서 중복 조건식 지정 (완료)
             if User.objects.filter(email=data['email']).exists():
-                return JsonResponse({'massage': 'KEY_ERROR(email_exists)'}, status=400)
+                return JsonResponse({'massage': 'KEY_ERROR_EMAIL_EXIST)'}, status=400)
 
             elif User.objects.filter(mobile=data['mobile']).exists():
-                return JsonResponse({'massage': 'KEY_ERROR(mobile_exists)'}, status=400)
+                return JsonResponse({'massage': 'KEY_ERROR_MOBILE_EXIST)'}, status=400)
 
             elif User.objects.filter(nickname=data['nickname']).exists():
-                return JsonResponse({'massage': 'KEY_ERROR(nickname_exists)'}, status=400)
+                return JsonResponse({'massage': 'KEY_ERROR_NICKNAME_EXIST)'}, status=400)
 
-            # email에 @, . 이 없는 경우 에러 발생 (완료)
-            # (질문) '@' or '.' not in email: @ 또는 . 이 없는경우 
-            # 에러 발생하란 조건을 줬지만 @와 .이 다 있는 상황에도 에러가 발생하였음
-            elif '@' not in email or '.' not in email:
+            # 이메일 정규식 사용 (email validation)
+            elif not email_regex.match(email):
                 return JsonResponse({'message':'PLEASE ENTER @ or .'}, status=400)
 
             # password가 8자리 미만인 경우 에러 발생 (완료)
             elif len(data['password']) < 8:
-                return JsonResponse({'massage': 'KEY_ERROR(password)'}, status=400)
+                return JsonResponse({'massage': 'KEY_ERROR_PASSWORD'}, status=400)
 
             # 위 조건식이 모두 아닌 경우 아래의 데이터 생성
             else:
@@ -47,4 +47,4 @@ class SignupView(View):
             return JsonResponse({'massage': 'SUCCESS'}, status=201)
         
         except KeyError:
-            return JsonResponse({'massage': 'KEY_ERROR(key)'}, status=400)
+            return JsonResponse({'massage': 'KEY_ERROR'}, status=400)

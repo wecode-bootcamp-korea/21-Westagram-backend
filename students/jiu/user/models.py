9678ad6bda0a280ab04e84d5e6d6207948a79286
ttakku-park <ttakku.park@gmail.com>
2021-05-31 21:43:01 +0900

    Add : view 작성, models 변경, migration 파일 삭제

--- students/jiu/user/models.py
+++ students/jiu/user/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
 
-# Create your models here.
 class User(models.Model) :
 
-    nickname    = models.CharField(max_length=30)
-    email       = models.EmailField(max_length=50, unique=True, null=False)
-    password    = models.CharField(max_length=20,  unique=True, null=False)
-    phonenumber = models.CharField(max_length=11,  unique=True)
+    nickname     = models.CharField(max_length=30,  unique=True)
+    email        = models.EmailField(max_length=50, unique=True, null=False)
+    password     = models.CharField(max_length=20,  null=False)
+    phone_number = models.CharField(max_length=11,  unique=True, null=False)
 
     class Meta:
         db_table = 'users'
--- students/jiu/user/views.py
+++ students/jiu/user/views.py
@@ -1,9 +1,8 @@
 import json
-
-from django.views import View
-from django.http  import JsonResponse
-from django.db    import IntegrityError
-from .models      import User
+from django.views     import View
+from django.http      import JsonResponse
+from django.db.models import Q
+from .models          import User
 
 
 class NewUserView(View) :
@@ -12,22 +11,37 @@ class NewUserView(View) :
 
         try:
             data = json.loads(request.body)
+  
+            # nickname, email, phone_number 중복 error
+            if User.objects.filter(
+                Q(nickname    =data['nickname']) or
+                Q(email       =data['email'])    or
+                Q(phone_number=data['phone_number'])
+            ).exists() :
+                return JsonResponse({'message':'ALREADY_EXISTS'}, status=400)
 
+            # email address error ('@','.')
+            if '@' not in data['email'] or '.' not in data['email'] :
+                return JsonResponse({'message':'WRONG_EMAIL_ERROR'}, status=400)
+            
+            # password min-length error (len < 8)
+            if 8 > len(data['password'])  :
+                return JsonResponse({'message':'PW_MINLENGTH_ERROR'}, status=400) 
+
+            # create
             User.objects.create(
-
-                nickname   = data['nickname'],
-                email      = data['email'],
-                password   = data['password'],
-                phonenumber= data['phonenumber']
-
+                nickname    =data['nickname'],
+                email       =data['email'],
+                password    =data['password'],
+                phone_number=data['phone_number']
                 )
             return JsonResponse({'message':'SUCCESS'}, status=201)
 
+            
         except KeyError :
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
 
-        except IntegrityError : # 중복예외처리
-            return JsonResponse({"message": "INTERGRITY_ERROR"}, status=400)
+        
 
         
 

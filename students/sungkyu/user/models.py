acb3e038dc580ad8ad634ade66146343f73b417b
김성규 <castleq37@gmail.com>
2021-05-31 18:59:48 +0900

    [김성규] Mission 2: 회원가입 엔드포인트 완료

--- students/sungkyu/user/models.py
+++ students/sungkyu/user/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
-class Users(models.Model):
-    email       = models.EmailField(max_length=50)
-    password    = models.CharField(max_length=45)
-    phonenumber = models.CharField(max_length=45)
-    nickname    = models.CharField(max_length=45)
-
+class User(models.Model):
+    email        = models.EmailField(unique=True, max_length=50)
+    password     = models.CharField(max_length=45)
+    phone_number = models.CharField(max_length=45)
+    nickname     = models.CharField(max_length=45)
 
     class Meta:
         db_table = 'users'
--- students/sungkyu/user/views.py
+++ students/sungkyu/user/views.py
@@ -1,61 +1,57 @@
 import json, re
 
-from django.views import View
-from django.http  import JsonResponse
+from django.views     import View
+from django.http      import JsonResponse
+from django.db.models import Q
 
-from .models      import Users
+from .models          import User
 
 class SignupView(View):
     def post(self, request):
 
-        user_email           = "(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
-        user_phonenumber     = '^[0-9]{3}-[0-9]{4}-[0-9]{4}$'
-        user_password        = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,18}$'
-
-        data                 = json.loads(request.body)
-        email_savedata       = Users.objects.filter(email = data['email'])
-        phonenumber_savedata = Users.objects.filter(phonenumber = data['phonenumber'])
-        nickname_savedata    = Users.objects.filter(nickname = data['nickname'])
-        
-        # email과 password의 값이 들어있지 않은 경우
-        # email과 password의 KEY값이 일치하지 않을 경우 keyerror 리턴
         try:
+            user_email            = "(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+            user_phone_number     = '^[0-9]{3}-[0-9]{4}-[0-9]{4}$'
+            user_password         = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,18}$'
+
+            data                   = json.loads(request.body)
+            
+            # email과 password의 값이 들어있지 않은 경우
+            # email과 password의 KEY값이 일치하지 않을 경우 keyerror 리턴
             if '' == data['email']:
                 return JsonResponse({'message': 'INVALID_EMAI'}, status=400)
 
-            elif '' == data['password']:
+            if '' == data['password']:
                 return JsonResponse({'message': 'INVALID_PASSWORD'}, status=400)
+
+            # email과 password, phone_number가 정규표현식에 부합하지 않는 경우
+            if not re.match(user_email, data['email']):
+                return JsonResponse({'message': 'INVALID_EMAIL'}, status=400)
+
+            if not re.match(user_phone_number, data['phone_number']):
+                return JsonResponse({'message': 'INVALID_PHONE_NUMBER'}, status=400)
+
+            if re.match(user_password, data['password']):
+                return JsonResponse({'message': 'INVALID_PASSWORD'}, status=400)
+
+            # 중복검사
+            if User.objects.filter(
+                Q(email=data['email'])|
+                Q(phone_number=data['phone_number'])|
+                Q(nickname=data['nickname'])).exists():
+                return JsonResponse({'MESSAGE' : 'OVERLAP_ERROR'}, status=400)
+
+            User.objects.create(
+                email        = data['email'],
+                password     = data['password'],
+                phone_number = data['phone_number'],
+                nickname     = data['nickname'],
+            )
+
+            return JsonResponse({'message': 'SUCCESS'}, status=201)
+
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
-
-        # email과 password, phonenumber가 정규표현식에 부합하지 않는 경우
-        if not re.match(user_email, data['email']):
-            return JsonResponse({'message': 'INVALID_EMAIL'}, status=400)
-
-        elif not re.match(user_phonenumber, data['phonenumber']):
-            return JsonResponse({'message': 'INVALID_PHONENUMBER'}, status=400)
-
-        elif re.match(user_password, data['password']):
-            return JsonResponse({'message': 'INVALID_PASSWORD'}, status=400)
-
-        # 중복검사
-        if   email_savedata.count() == 1:
-            return JsonResponse({'MESSAGE' : 'EMAIL_OVERLAP_ERROR'}, status=400)
-
-        elif phonenumber_savedata.count() == 1:
-            return JsonResponse({'MESSAGE' : 'POHNENUMBER_OVERLAP_ERROR'}, status=400)
-
-        elif nickname_savedata.count() == 1:
-            return JsonResponse({'MESSAGE' : 'NICKNAME_OVERLAP_ERROR'}, status=400)
-
-        Users.objects.create(
-            email       = data['email'],
-            password    = data['password'],
-            phonenumber = data['phonenumber'],
-            nickname    = data['nickname'],
-        )
-
-        return JsonResponse({'message': 'SUCCESS'}, status=201)
 
     def get(self, request):
         return JsonResponse({'message': 'SUCCESS'}, status=200)

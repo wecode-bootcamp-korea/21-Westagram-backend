9b65b72d2ccb21577762c7a4db813a12ec4bf6d6
김성규 <castleq37@gmail.com>
2021-06-01 18:23:59 +0900

    [김성규] mission3 로그인엔드포인트 완료

--- students/sungkyu/user/models.py
+++ students/sungkyu/user/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
     email        = models.EmailField(unique=True, max_length=50)
-    password     = models.CharField(max_length=45)
+    password     = models.CharField(max_length=300)
     phone_number = models.CharField(max_length=45)
     nickname     = models.CharField(max_length=45)
 
--- students/sungkyu/user/views.py
+++ students/sungkyu/user/views.py
@@ -1,10 +1,14 @@
+import jwt
+import bcrypt
 import json, re
 
-from django.views     import View
-from django.http      import JsonResponse
-from django.db.models import Q
+from django.http.response import HttpResponse
+from django.views         import View
+from django.http          import JsonResponse
+from django.db.models     import Q
 
-from .models          import User
+from westagram.settings   import SECRET_KEY
+from .models              import User
 
 class SignupView(View):
     def post(self, request):
@@ -14,7 +18,7 @@ class SignupView(View):
             user_phone_number     = '^[0-9]{3}-[0-9]{4}-[0-9]{4}$'
             user_password         = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,18}$'
 
-            data                   = json.loads(request.body)
+            data                  = json.loads(request.body)
             
             # email과 password의 값이 들어있지 않은 경우
             # email과 password의 KEY값이 일치하지 않을 경우 keyerror 리턴
@@ -41,9 +45,13 @@ class SignupView(View):
                 Q(nickname=data['nickname'])).exists():
                 return JsonResponse({'MESSAGE' : 'OVERLAP_ERROR'}, status=400)
 
+            # password 암호화
+            password        = data['password'].encode('utf-8')
+            password_bcrypt = bcrypt.hashpw(password, bcrypt.gensalt()).decode('utf-8')
+
             User.objects.create(
                 email        = data['email'],
-                password     = data['password'],
+                password     = password_bcrypt,
                 phone_number = data['phone_number'],
                 nickname     = data['nickname'],
             )
@@ -53,6 +61,22 @@ class SignupView(View):
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
 
-    def get(self, request):
-        return JsonResponse({'message': 'SUCCESS'}, status=200)
+class SigninView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
 
+            if not User.objects.filter(email=data['email']).exists():
+                return JsonResponse({"message": "IVALID_USER"}, status=401)
+            user = User.objects.get(email=data['email'])
+
+            # 비밀번호 확인
+            if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+                # 토큰 발행
+                token = jwt.encode({'eamil' : data['email']}, SECRET_KEY, algorithm="HS256")
+                return JsonResponse({"token": token}, status=200)
+            else:
+                return JsonResponse({"message": "INVALID_USER"},status=401)
+
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)

83d13ade2d6398de921ee3549adc2c01af95faa6
박준영 <sjwm98@gmail.com>
2021-06-01 12:20:00 +0900

    Create: 로그인 엔드포인트 작업중

--- students/junyeong/user/urls.py
+++ students/junyeong/user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from .views import SignupView
+from .views import LoginView
 # from .views import LoginView
 
 urlpatterns = [
-    path('sign', SignupView.as_view()),
-    # path('login', LoginView.as_view())
+    path('/sign', SignupView.as_view()),
+    path('/login', LoginView.as_view())
     
 ]

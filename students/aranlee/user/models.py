4db80ea9791c33ccef0b182e01dd5f8a25b44653
AAAAA Ran <aaaaaran@AAAAAui-MacBookPro.local>
2021-06-01 13:46:43 +0900

    리뷰 수정 반영

--- students/aranlee/user/models.py
+++ students/aranlee/user/models.py
@@ -1,16 +1,11 @@
 from django.db import models
 
-
 class User(models.Model):
-    user_email = models.CharField(max_length=40, default=None, unique=True)
-    password   = models.CharField(max_length=30, default=None)
-    phone      = models.CharField(max_length=50, blank=True)
-    nickname   = models.CharField(max_length=50, blank=True)
+    email    = models.CharField(max_length=40, unique=True)
+    password = models.CharField(max_length=30)
+    phone    = models.CharField(max_length=50, blank=True)
+    nickname = models.CharField(max_length=50, blank=True)
 
     class Meta:
-        db_table = 'users' 
-
-        
-
-
+        db_table = 'users'        
         
--- students/aranlee/user/views.py
+++ students/aranlee/user/views.py
@@ -1,58 +1,46 @@
 import json,re
 
-from django.views import View
-from django.http import JsonResponse
+from django.views     import View
+from django.http      import JsonResponse
+from django.db.models import Q
 
-from .models import User
+from .models          import User
 
 class SignUpView(View):
     def post(self, request):
-     
         data = json.loads(request.body)
-        
-
         try:
-            user_email = data['user_email']
-            password   = data['password']
-            phone      = data['phone']
-            nickname   = data['nickname']
-            
-            validation_email = '^[A-Za-z0-9\.+_-]+\@[A-Za-z0-9\._-]+\.[a-zA-Z]+$' 
+            email            = data['email']
+            password         = data['password']
+            phone            = data['phone']
+            nickname         = data['nickname']   
+            validation_email = '^\w+\@\w+\.\D+$'
 
-        
-            if '' == user_email:
+            q = Q()
+
+            if '' == email:
                 return JsonResponse({'message': 'USER_ID_IS_EMPTY'}, status=400)
-            if User.objects.filter(user_email = user_email).exists():
-                return JsonResponse({'message': 'USER_ID_EXISTS'}, status=400)
-            if not re.match (validation_email, user_email)  :
+            if not re.match (validation_email, email):
                 return JsonResponse({'message': 'INVALID_EMAIL_FORMAT'}, status=400)
             if '' == password:
                 return JsonResponse({'message': 'PASSWORD_IS_EMPTY'}, status=400)
             if len(password) < 8:
                 return JsonResponse({'message': 'TOO_SHORT_PASSWORD'}, status=400) 
-            if User.objects.filter(phone = phone).exists():
-                return JsonResponse({'message': 'PHONE_NUMBER_EXISTS'}, status=400)
-            if User.objects.filter(nickname = nickname).exists():
-                return JsonResponse({'message': 'NICK_NAME_EXISTS'}, status=400) 
-            
-            
-            else:
-                User.objects.create(
-                    user_email  = user_email,
-                    password = password, 
-                    phone    = phone,
-                    nickname = nickname
-                )
-                return JsonResponse({'message': 'SUCCESS'} , status=201)
-    
+            if User.objects.filter(Q(email = email) | Q(phone = phone) | Q(nickname = nickname)).exists():
+                return JsonResponse({'message': 'ALREADY_EXISTS'}, status=400)
+        
+            User.objects.create(
+                email    = email,
+                password = password, 
+                phone    = phone,
+                nickname = nickname
+            )
+            return JsonResponse({'message': 'SUCCESS'}, status=201)
         except KeyError: 
             return JsonResponse({'message': 'KEY_ERROR'}, status=400) 
         except User.DoesNotExist:
             return JsonResponse ({'message':'USER_DOSE_NOT_EXIST'}, status=400)
-        except Exception as e:
-            return JsonResponse({"message": "UNKNOWN_ERROR"}, status=400)
-        
-            
+       
 
     def get(self, request):
         user = User.objects.all()

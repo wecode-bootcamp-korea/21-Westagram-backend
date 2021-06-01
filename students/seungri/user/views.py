fb43b0b30b4fdbc71132670e37c1b71ecf8620e9
최승리 <apol.lun058@gmail.com>
2021-06-01 14:30:55 +0900

    [최승리] Mission3 : 로그인 엔드포인트 완료

--- students/seungri/user/views.py
+++ students/seungri/user/views.py
@@ -1,24 +1,21 @@
-import json
-import re
-import bcrypt
-import jwt
-
+import json, re, bcrypt, jwt
 
 from django.views import View
 from django.http  import JsonResponse
 from django.db    import IntegrityError
 from django.core.exceptions import ObjectDoesNotExist
 
+from westagram.settings import SECRET_KEY, ALGORITHM
 from .models import User
 
 class UserJoinIn(View):
     def post(self, request):
         try:
             data      = json.loads(request.body)
-            email     = data["email"]
-            password  = data["password"]
-            phone_num = data["phone_num"]
-            nick_name = data["nick_name"]
+            email     = data['email']
+            password  = data['password']
+            phone_num = data['phone_num']
+            nick_name = data['nick_name']
             
             email_regex = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
 
@@ -28,14 +25,9 @@ class UserJoinIn(View):
             if len(password) < 8 or len(password) > 15:
                 return JsonResponse({"message": "PASSWORD_ERROR"}, status=400)
 
-            # 1. salt = bcrypt.gensalt() # 소금생성(난수생성)
-            # 2. password = password.encode('utf-8') # bcrypt 인자는 bytes여야 하니 인코드
-            # 3. hashed_password = bcrypt.hashpw(password, salt) # 암호화
-            # 4. decoded_password = hashed_password.decode('utf-8') # DB에 저장전 다시 디코드 bytes->utf-8
-
             User.objects.create(
                 email     = email,
-                password  = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'), # 코드 간소화
+                password  = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
                 phone_num = phone_num,
                 nick_name = nick_name
                 )
@@ -48,20 +40,24 @@ class UserJoinIn(View):
 class UserLogIn(View):
     def post(self, request):
         try:
-            data      = json.loads(request.body)
-            email     = data["email"]
-            password  = data["password"]
-            input_password  = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            data            = json.loads(request.body)
+            email           = data['email']
+            input_password  = data['password'].encode('utf-8')
 
             if email == "" or input_password == "":
                 return JsonResponse({"message": "KEY_ERROR"}, status=400)
 
-            login_user = User.objects.get(email=email)
+            login_user  = User.objects.get(email=email)
             db_password = login_user.password
             db_password = db_password.encode('utf-8')
             
-            if bcrypt.checkpw(input_password, db_password):
-                return JsonResponse({"message": "SUCCESS!"}, status=200)
+            if not bcrypt.checkpw(input_password, db_password):
+                return JsonResponse({"message": "INVALID_USER"}, status=401)
+
+            user      = {"user_id" : login_user.id}
+            token     = jwt.encode(user, SECRET_KEY, ALGORITHM)
+            token_str = token.decode('utf-8')
+            return JsonResponse({"token" : token_str, "message" : "SUCCESE!"}, status=200)
 
         except ObjectDoesNotExist:
             return JsonResponse({"message": "INVALID_USER"}, status=401)

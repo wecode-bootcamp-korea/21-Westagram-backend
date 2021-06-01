d65b249f1366957538b398158b6024a908bc124c
HWANG BOK SIL <tlatms0908@gmail.com>
2021-06-01 21:22:00 +0900

    Add: SigninView 작성 1. 로그인 시 저장된 정보와 맞는지 확인하는 기능 구현 2. 로그인 시 입력된 패스워드 bcrypt 기능 구현

--- students/boksil/users/views.py
+++ students/boksil/users/views.py
@@ -12,7 +12,8 @@ class SignupView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
-            bcrypt_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+
+            bcrypt_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
             if User.objects.filter(mobile=data['mobile']).exists():
                 return JsonResponse({'massage': 'KEY_ERROR_MOBILE_EXIST'}, status=400)
@@ -40,5 +41,30 @@ class SignupView(View):
         
         except IntegrityError:
             return JsonResponse({'massage': 'KEY_ERROR_EMAIL_EXIST'}, status=400)
+
+
+class SigninView(View):
+    def post(self, request):
+        try:
+            data           = json.loads(request.body)
+            input_email    = data['email']
+            input_password = data['password'].encode('utf-8')
+
+            if input_email == '' or input_password == '':
+                return JsonResponse({'message': 'INVALID_USER'}, status=401)
+
+            db_email = User.objects.get(email=input_email)
+            db_password = db_email.password # 가져온 이메일의 패스워드
+            db_password = db_password.encode('utf-8') # 패스워드를 다시 encode
+
+            if not bcrypt.checkpw(input_password, db_password):
+                return JsonResponse({'message': 'INVALID_USER'}, status=401)
+
+            return JsonResponse({'message': 'SUCCESS'}, status=200)
+
+        except KeyError:
+            return JsonResponse({'massage': 'KEY_ERROR'}, status=400)
+        
+
 
 

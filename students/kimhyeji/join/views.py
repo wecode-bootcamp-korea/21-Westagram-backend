7afeaf59cdb07fc4150e297e4d3c78931e5de5ee
kimhyeji <hyjias@naver.com>
2021-06-01 19:08:02 +0900

    회원가입 엔드포인트수정1

--- students/kimhyeji/join/views.py
+++ students/kimhyeji/join/views.py
@@ -1,40 +1,41 @@
-#from django.shortcuts import render
 import json
+import bcrypt
 
 from django.http      import JsonResponse
 from django.views     import View
-from django.db.models import Q
 
 from .models import User
 
 # Create your views here.
 
-class JoinView(View):
+class UserView(View):
     def post(self, request):
+
         try:
             data = json.loads(request.body)
-            
-            if User.objects.filter(Q(password=data['password']) | Q(email=data['email'])):
-                return JsonResponse({'message':'중복되었습니다.'}, status=400)
+            emailcheck = "^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$" 
 
-            if len(data['password']) < 8 :
-                return JsonResponse({"message":"8자리이상입력하세요."}, status=400)
-
-            if data['email'] in ('@' or '.') :
+            if re.match(emailcheck, data['email']):
                 return JsonResponse({"message":"이메일형식이잘못되었습니다."}, status=400)
 
-            else:
-                User.objects.create(
+            if User.objects.filter(email=data['email']).exist():
+                return JsonResponse({'message':'중복되었습니다.'}, status=400)
+
+            # if len(data['password']) < 8 :
+            #     return JsonResponse({"message":"8자리이상입력하세요."}, status=400)
+            password = data['password']
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+   
+            User.objects.create(
                     email        = data['email'],
                     phone_number = data.get('phone_number'),
                     password     = data['password'], 
                     nick_name    = data.get('nick_name')
                 )
-                return JsonResponse({'message':'SUCCESS'}, status=200)
+            return JsonResponse({'message':'SUCCESS'}, status=201)
 
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
 
-                        
 
             

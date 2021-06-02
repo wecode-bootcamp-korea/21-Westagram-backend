022ffa953ffd9078e0adf766e73063c9a65e0fc5
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2021-06-02 14:13:23 +0900

    Create: 게시판 get, post method 구현

--- students/hyojin/westagram/settings.py
+++ students/hyojin/westagram/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 from my_settings import SECRET_KEY, DATABASES 
@@ -27,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Application definition
 
@@ -38,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'user'
+    'user',
+    'post'
 ]
 
 MIDDLEWARE = [

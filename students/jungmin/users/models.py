1d5f17ae3ef5d50759cc5efee5141c107ded99d6
이정민 <dlwjdals5600@gmail.com>
2021-06-02 13:54:39 +0900

    로그인 및 비밀번호 암호화 완료

--- students/jungmin/users/models.py
+++ students/jungmin/users/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     user_name    = models.CharField(max_length=45, unique=True)
     phone_number = models.CharField(max_length=45, unique=True)
     user_email   = models.CharField(max_length=50, unique=True, null=False)
-    password     = models.CharField(max_length=45, null=False)
+    password     = models.CharField(max_length=200, null=False)
 
     class Meta:
         db_table = "users"
--- students/jungmin/users/views.py
+++ students/jungmin/users/views.py
@@ -1,13 +1,17 @@
-import json ,re
+import json, re, bcrypt, jwt
 
 from django.views     import View
 from django.http      import JsonResponse
 from django.db.models import Q
 
 from .models          import User
+from my_settings      import SECRET_KEY, JWT_ALGORITHM
 
 re_email    = '^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'
 re_password = '^[A-Za-z\d$@$!%^()*#?&]{8,}$'
+
+
+
 
 class UserView(View):
     def post(self, request):
@@ -24,23 +28,42 @@ class UserView(View):
             if not re.match(re_password, password):
                 return JsonResponse({"message":"The password is not appropriate"}, status=400)
 
-            if not user_email or not password:
-                return JsonResponse({"message":"KEY_ERROR"}, status=400)
-
             if User.objects.filter(
                 Q(user_name    = user_name)|
                 Q(phone_number = phone_number)|
                 Q(user_email   = user_email)).exists():
                 return JsonResponse({"message": "DUPLICATED_CLIENT_INFORMATION"}, status=409)
-            
+
+            hash_pw = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
             User.objects.create(
                 user_name    = user_name,
                 phone_number = phone_number,
                 user_email   = user_email,
-                password     = password
+                password     = hash_pw
             )
             return JsonResponse({'message':'SUCCESS'}, status = 201)
 
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status = 400)
 
+class LoginView(View):    
+    def post(self,request):
+        try:
+            data       = json.loads(request.body)
+            user_email = data['user_email']
+            password   = data['password']
+
+            if not User.objects.filter(user_email = user_email).exists():
+                return JsonResponse({'message':'INVALID_USER'}, status = 401)
+            
+            user = User.objects.get(user_email = data['user_email'])
+
+            if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                return JsonResponse({'message':'PASSWORD INCORRECT!!'}, status = 401)
+            
+            return JsonResponse({'message':'LOGIN SUCCESS!!!',
+                                   'Token': jwt.encode({'id':user.id}, SECRET_KEY, algorithm=JWT_ALGORITHM)}, status = 200)
+
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR!!'}, status = 400)

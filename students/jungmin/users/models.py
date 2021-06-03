75e4b6a71bcebc8d75173f9a2d6767f6575897df
이정민 <dlwjdals5600@gmail.com>
2021-06-03 12:04:39 +0900

    공백 제거

--- students/jungmin/users/models.py
+++ students/jungmin/users/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class User(models.Model):
     user_name    = models.CharField(max_length=45, unique=True)
     phone_number = models.CharField(max_length=45, unique=True)
--- students/jungmin/users/views.py
+++ students/jungmin/users/views.py
@@ -10,9 +10,6 @@ from my_settings      import SECRET_KEY, JWT_ALGORITHM
 re_email    = '^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'
 re_password = '^[A-Za-z\d$@$!%^()*#?&]{8,}$'
 
-
-
-
 class UserView(View):
     def post(self, request):
         try:
@@ -53,17 +50,17 @@ class LoginView(View):
             data       = json.loads(request.body)
             user_email = data['user_email']
             password   = data['password']
-
+            
             if not User.objects.filter(user_email = user_email).exists():
                 return JsonResponse({'message':'INVALID_USER'}, status = 401)
             
             user = User.objects.get(user_email = data['user_email'])
-
+            
             if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
                 return JsonResponse({'message':'PASSWORD INCORRECT!!'}, status = 401)
             
             return JsonResponse({'message':'LOGIN SUCCESS!!!',
-                                   'Token': jwt.encode({'id':user.id}, SECRET_KEY, algorithm=JWT_ALGORITHM)}, status = 200)
+                                   'Token': jwt.encode({'id':user.id}, SECRET_KEY, JWT_ALGORITHM)}, status = 200)
 
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR!!'}, status = 400)

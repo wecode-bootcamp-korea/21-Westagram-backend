aa9a189d46593fcfd7d175fc2a092130ffb108e2
최승리 <apol.lun058@gmail.com>
2021-06-03 10:03:38 +0900

    [최승리] Mission3 : 로그인 엔드포인트-리뷰수정

--- students/seungri/user/views.py
+++ students/seungri/user/views.py
@@ -48,16 +48,13 @@ class UserLogIn(View):
                 return JsonResponse({"message": "KEY_ERROR"}, status=400)
 
             login_user  = User.objects.get(email=email)
-            db_password = login_user.password
-            db_password = db_password.encode('utf-8')
+            db_password = login_user.password.encode('utf-8')
             
-            if not bcrypt.checkpw(input_password, db_password):
+            if not bcrypt.checkpw(input_password, db_password):  
                 return JsonResponse({"message": "INVALID_USER"}, status=401)
 
-            user      = {"user_id" : login_user.id}
-            token     = jwt.encode(user, SECRET_KEY, ALGORITHM)
-            token_str = token.decode('utf-8')
-            return JsonResponse({"token" : token_str, "message" : "SUCCESE!"}, status=200)
+            token     = jwt.encode({"user_id" : login_user.id}, SECRET_KEY, algorithm=ALGORITHM)
+            return JsonResponse({"token" : token, "message" : "SUCCESE!"}, status=200)
 
         except ObjectDoesNotExist:
             return JsonResponse({"message": "INVALID_USER"}, status=401)
--- students/seungri/westagram/urls.py
+++ students/seungri/westagram/urls.py
@@ -18,4 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('user', include('user.urls')),
+    # path('postings', include('postings.urls'))
 ]

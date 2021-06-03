8f2acd8546f8562fa3394a899ce8b64bcb2325b9
HWANG BOK SIL <tlatms0908@gmail.com>
2021-06-03 11:23:55 +0900

    Modify: 리뷰 수정사항 적용 1. db_password 변수 간략화 2. 필요없는 공백 삭제 3. token byte 타입으로 전송

--- students/boksil/users/models.py
+++ students/boksil/users/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class User(models.Model):
     password = models.CharField(max_length=90) 
     email    = models.EmailField(max_length=45, unique=True)
-    mobile   = models.CharField(max_length=20, null=True, blank=False) # null=True
-    nickname = models.CharField(max_length=45, null=True, blank=False) # null=True
+    mobile   = models.CharField(max_length=20, null=True) # null=True
+    nickname = models.CharField(max_length=45, null=True) # null=True
 
     class Meta:
         db_table = 'users'
--- students/boksil/users/views.py
+++ students/boksil/users/views.py
@@ -16,16 +16,16 @@ class SignupView(View):
             bcrypt_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
             if User.objects.filter(mobile=data['mobile']).exists():
-                return JsonResponse({'massage': 'KEY_ERROR_MOBILE_EXIST'}, status=400)
+                return JsonResponse({'massage': 'MOBILE_EXIST'}, status=400)
 
             if User.objects.filter(nickname=data['nickname']).exists():
-                return JsonResponse({'massage': 'KEY_ERROR_NICKNAME_EXIST'}, status=400)
+                return JsonResponse({'massage': 'NICKNAME_EXIST'}, status=400)
 
             if not email_regex.match(data['email']):
                 return JsonResponse({'message':'PLEASE ENTER @ or .'}, status=400)
 
             if len(data['password']) < 8:
-                return JsonResponse({'massage': 'KEY_ERROR_PASSWORD'}, status=400)
+                return JsonResponse({'massage': 'PASSWORD OF LEAST 8 CHARACTERS'}, status=400)
 
             User.objects.create(
                 email    = data['email'],
@@ -40,12 +40,11 @@ class SignupView(View):
             return JsonResponse({'massage': 'KEY_ERROR'}, status=400)
         
         except IntegrityError:
-            return JsonResponse({'massage': 'KEY_ERROR_EMAIL_EXIST'}, status=400)
-
+            return JsonResponse({'massage': 'INTEGRITY_ERROR'}, status=400)
 
 class SigninView(View):
     def post(self, request):
-        try:
+        try: 
             data           = json.loads(request.body)
             input_email    = data['email']
             input_password = data['password'].encode('utf-8')
@@ -54,21 +53,15 @@ class SigninView(View):
                 return JsonResponse({'message': 'INVALID_USER'}, status=401)
 
             db_email    = User.objects.get(email=input_email)
-            db_password = db_email.password # 가져온 이메일의 패스워드
-            db_password = db_password.encode('utf-8') # 패스워드를 다시 encode
+            db_password = db_email.password.encode('utf-8') # 가져온 이메일의 패스워드를 다시 encode
 
             if not bcrypt.checkpw(input_password, db_password):
                 return JsonResponse({'message': 'INVALID_USER'}, status=401)
             
-            user      = {'user_id': db_email.id}
-            token     = jwt.encode(user, SECRET_KEY, ALGORITHM)
-            token_num = jwt.decode(token, SECRET_KEY, ALGORITHM)
+            token = jwt.encode({'user_id': db_email.id}, SECRET_KEY, ALGORITHM)
 
-            return JsonResponse({'token': token_num, 'message': 'SUCCESS'}, status=200)
+            return JsonResponse({'token': token, 'message': 'SUCCESS'}, status=200)
 
         except KeyError:
             return JsonResponse({'massage': 'KEY_ERROR'}, status=400)
         
-
-
-

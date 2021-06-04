4417460a255cebf2b648027f29a5dbe556f1980c
chp9419 <chp.code@gmail.com>
2021-06-04 19:28:29 +0900

    Add:board.View 17~18번 코드삭제

--- students/chaghyunpark/board/views.py
+++ students/chaghyunpark/board/views.py
@@ -15,17 +15,15 @@ class PostView(View):
             data  = json.loads(request.body)
             token = request.headers['token']
             aut = jwt.decode(token,SECRET_KEY,ALGORITHM)
-         
-            if not User.objects.filter(email=aut['email']).exists():
-                return JsonResponse({'이메일형식 및 닉네임 정보가 맞지 않습니다.'},status=400)
+            # if not User.objects.filter(email=aut['email']).exists():
+            #     return JsonResponse({'이메일형식 및 닉네임 정보가 맞지 않습니다.'},status=400)
             user = User.objects.get(email=aut['email'])
             Postboard.objects.create(
                 user         = user,
                 contensboard = data['contens'],
                 img_url      = data['imgurl'])
             return JsonResponse({'MESSAGE':'성공적으로 끝났습니다.'},status=201)
-
-           
+            
          except KeyError:
             return JsonResponse({'message':'키에러'},status=400)
         
